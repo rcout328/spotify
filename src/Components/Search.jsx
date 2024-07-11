1a910e03d8f189d5e25b2874b938700fb05dc6da
rcout328 <solovpxoffical@gmail.com>
2024-07-12 04:47:18 +0530

    c

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,187 +1,54 @@
-import { useState, useEffect, useContext } from "react";
-import { Link } from "react-router-dom";
-import { FaSearch } from "react-icons/fa";
-import { DarkContext } from "./Context/DarkProvider";
+import React, { useState } from "react";
+import axios from "axios";
 
 const Search = () => {
-  // State variables
-  const [songs, setSongs] = useState([]);
-  const [search, setSearch] = useState("");
-  const [speechRecognition, setSpeechRecognition] = useState(null);
-  const [isListening, setIsListening] = useState(false);
-  const [showListeningPopup, setShowListeningPopup] = useState(false);
-  const [dark] = useContext(DarkContext);
+  const [message, setMessage] = useState("");
+  const [predictedResult, setPredictedResult] = useState("");
 
-  // Fetch data from Spotify based on the search query
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        // Spotify API details
-        const clientId = "your_client_id";
-        const clientSecret = "your_client_secret";
-        const tokenEndpoint = "https://accounts.spotify.com/api/token";
-        const apiEndpoint = "https://api.spotify.com/v1/search";
-
-        // Fetch access token
-        const tokenResponse = await fetch(tokenEndpoint, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-            Authorization: `Basic ${btoa(`${clientId}:${clientSecret}`)}`,
-          },
-          body: "grant_type=client_credentials",
-        });
-
-        const tokenData = await tokenResponse.json();
-
-        const params = new URLSearchParams({
-          q: search,
-          type: "track",
-          market: "ES",
-          limit: 10,
-          offset: 5,
-          include_external: "audio",
-        });
-
-        // Fetch Spotify data using the access token and search parameters
-        const response = await fetch(`${apiEndpoint}?${params.toString()}`, {
-          method: "GET",
-          headers: {
-            Authorization: `Bearer ${tokenData.access_token}`,
-          },
-        });
-
-        if (response.ok) {
-          const data = await response.json();
-          setSongs(data.tracks.items);
-        } else {
-          console.error("Error fetching data");
-        }
-      } catch (error) {
-        console.error("Error in the fetch operation", error);
-      }
-    };
-
-    if (search) {
-      fetchData();
-    }
-  }, [search]);
-
-  // Initialize speech recognition
-  useEffect(() => {
-    const initializeSpeechRecognition = () => {
-      const recognition = new (window.SpeechRecognition ||
-        window.webkitSpeechRecognition ||
-        window.mozSpeechRecognition ||
-        window.msSpeechRecognition)();
-
-      recognition.lang = "en-US";
-      recognition.interimResults = true;
-
-      recognition.onstart = () => {
-        setIsListening(true);
-        setShowListeningPopup(true);
-      };
-
-      recognition.onresult = (event) => {
-        let transcript = "";
-        for (let i = event.resultIndex; i < event.results.length; i++) {
-          if (event.results[i].isFinal) {
-            transcript += event.results[i][0].transcript;
-          } else {
-            transcript += event.results[i][0].transcript + " ";
-          }
-        }
-        setSearch(transcript);
-      };
-
-      recognition.onend = () => {
-        setIsListening(false);
-        setShowListeningPopup(false);
-      };
-
-      recognition.onerror = (err) => {
-        console.log("Error: ", err);
-        alert(
-          "Boss! You're not allowed to use this feature. Please allow voice permission"
-        );
-      };
-
-      setSpeechRecognition(recognition);
-    };
-
-    initializeSpeechRecognition();
-  }, []);
-
-  // Handle voice button click
-  const handleVoiceButtonClick = () => {
-    if (!isListening) {
-      speechRecognition.start();
-    } else {
-      speechRecognition.stop();
+  const predictMessage = async () => {
+    try {
+      const response = await axios.post("http://127.0.0.1:5001/predict", {
+        message: message,
+      });
+      setPredictedResult(response.data.predicted_result);
+    } catch (error) {
+      console.error("Error fetching data:", error);
     }
   };
 
   return (
-    <div className={` ${dark ? "bg-black" : "bg-white"} min-h-screen`}>
-      <div className="container mx-auto p-8">
-        <div className="relative">
-          {/* Search input */}
-          <input
-            type="text"
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-            placeholder="Search for songs..."
-            className="w-full px-4 py-2 rounded-md border-none shadow-md focus:outline-none focus:ring-2 focus:ring-green-500"
-          />
-          {/* Search icon */}
-          <FaSearch className="absolute right-3 top-3 text-gray-500" />
-        </div>
+    <div style={{ padding: 20 }}>
+      <h2>Scam or Ham Message Classifier</h2>
+      <textarea
+        rows={4}
+        placeholder="Enter message"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        style={{
+          width: "100%",
+          minHeight: 100,
+          borderWidth: 1,
+          borderColor: "#ccc",
+          padding: 10,
+          marginVertical: 10,
+          fontFamily: "Arial, sans-serif",
+        }}
+      />
 
-        {/* Voice search button */}
-        <div className="flex justify-center items-center">
-          <button
-            className="mt-4 bg-green-500 text-white py-2 px-4 rounded-md flex items-center justify-center"
-            onClick={handleVoiceButtonClick}
-          >
-            {isListening ? "Stop Listening" : "Start Listening"}
-          </button>
-        </div>
+      <button
+        style={{
+          padding: 10,
+          backgroundColor: "#007BFF",
+          color: "white",
+          border: "none",
+          cursor: "pointer",
+        }}
+        onClick={predictMessage}
+      >
+        Predict
+      </button>
 
-        {/* Listening popup */}
-        {showListeningPopup && (
-          <div className="listening-popup">
-            <p>Listening...</p>
-            <p>User said: {search}</p>
-          </div>
-        )}
-
-        {/* Display the songs information */}
-        {songs.length > 0 && (
-          <div className="mt-8 grid gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-            {songs.map((song, index) => (
-              <Link to={`/songdetails/${song.id}`} key={index}>
-                <div className="bg-white p-4 rounded-lg shadow-md transition duration-300 transform hover:scale-105">
-                  {/* Song details */}
-                  <img
-                    src={song.album.images[0].url}
-                    alt={song.name}
-                    className="w-full h-40 object-cover rounded-md mb-4"
-                  />
-                  <h2 className="text-xl font-bold text-gray-800 mb-2">
-                    {song.name}
-                  </h2>
-                  <p className="text-gray-600">
-                    Artist:{" "}
-                    {song.artists.map((artist) => artist.name).join(", ")}
-                  </p>
-                  {/* Add more song details as needed */}
-                </div>
-              </Link>
-            ))}
-          </div>
-        )}
-      </div>
+      {predictedResult && <p>Predicted result: {predictedResult}</p>}
     </div>
   );
 };
